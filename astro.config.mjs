c3139009e19275c942ea2b021a793700c9a25215
Anton Vetrov <antonneprosto@gmail.com>
2026-02-24 17:45:20 +0000

    Add sitemap.xml, fix robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
